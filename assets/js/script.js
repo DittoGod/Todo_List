8611f32f56e1a2a383d883a8d46ecf735f25fa31
DittoGod <dittogod1@gmail.com>
2019-08-13 12:31:25 +0100

    added ability to create new todos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
 // check of specific todos by clicking
-$("li").on("click", function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
 // click X to delete the todo (e) -> event
-$("span").on("click", function(e){
-    $(this).parent().fadeOut(500, function(){
+$("ul").on("click", "span", function (e) {
+    $(this).parent().fadeOut(500, function () {
+        // removes the element from the DOM
         $(this).remove();
     });
+    // stops the propegation of the event.
     e.stopPropagation();
+});
+
+// text input from user.
+$("input[type='text']").keypress(function (e) {
+    if (e.which === 13) {
+        // grab the text from the textbox and save it to a variable
+        var todoText = $(this).val();
+        // clear the contentsof the textbox.
+        $(this).val("");
+        // create new li and add to ul.
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
